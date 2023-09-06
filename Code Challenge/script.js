accd2f629601c016e663bc7d2891548dfdb6d274
MateoAmorin <140856359+MateoAmorin@users.noreply.github.com>
2023-09-06 16:44:28 -0300

    Add files via upload

---
+++ Code Challenge/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formulario = document.getElementById('miFormulario');
+    
+   formulario.addEventListener('submit', function (e) {
+        e.preventDefault(); 
+        
+        const nombreInput = document.getElementById('nombre');
+        const apellidoInput = document.getElementById('apellido');
+        const fechaNacimientoInput = document.getElementById('fechaNacimiento');
+   
+   
+        const datos = {
+            name: nombreInput.value,
+            username: apellidoInput.value,
+            birthdate: fechaNacimientoInput.value,
+        };
+        
+        const url = 'https://jsonplaceholder.typicode.com/users';
+        const opciones = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(datos),
+        };
+        
+    
+        fetch(url, opciones)
+            .then((response) => {
+                 return response.json();
+            })
+            .then((data) => {
+  
+                console.log('Respuesta del servidor:', data);
+            })
+         
+         }) ;
+});
